f0b3d760c6d1653e90054905f6f40e9c8f6dd55a
Israel Dominguez <isrra.usa@gmail.com>
2022-10-06 21:14:27 -0500

    se inicio desde cero copiando el codigo proporcionado

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Código de asignación
+var generateBtn = document.querySelector("#generate");
 
+// Escriba la contraseña en la entrada #password
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Agregar oyente de eventos para generar el botón
+generateBtn.addEventListener("click", writePassword);
